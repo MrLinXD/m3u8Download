c4146f78a4019eaad19ad92de7bdac384a7715a3
Mr.Lin <553515787@qq.com>
2024-10-24 00:02:29 +0800

    .

--- src/main/java/net/m3u8/JsoupExample.java
+++ src/main/java/net/m3u8/JsoupExample.java
@@ -6,27 +6,32 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-//@Slf4j
+import java.io.IOException;
+
+@Slf4j
 public class JsoupExample {
-    public static void main(String[] args) {
-        try {
-            // 使用Jsoup连接到指定的网址，并获取HTML内容
-            Document doc = Jsoup.connect("https://51cg.fun").get();
+    private static final String wqnr = "/archives.html";
 
-            System.out.println(doc.outerHtml());
-            // 通过选择器语法查找具体的元素
-            Elements titles = doc.select("h1");  // 查找所有<h1>元素
-            Element title = titles.first();  // 获取第一个<h1>元素
-            System.out.println("Title: " + title.text());
+    public static void main(String[] args) throws Throwable {
+//        testHtml("https://51cg.fun/");
+//        testHtml("https://aware.uxkzxzn.com" + wqnr);
+        testHtml("https://ht5wz2.pyktmhe.org/archives/172660/");
+    }
 
-            Elements links = doc.select("a");  // 查找所有<a>元素
-            for (Element link : links) {
-                String href = link.attr("href");  // 获取<a>元素的href属性值
-                String text = link.text();  // 获取<a>元素的文本内容
-                System.out.println("Link: " + text + " - " + href);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static void testHtml(String url) throws IOException {
+        // 使用Jsoup连接到指定的网址，并获取HTML内容
+        Document doc = Jsoup.connect(url).get();
+        log.debug("----------------------------------------\r\n{}\r\n----------------------------------------", doc.html());
+        // 通过选择器语法查找具体的元素
+        Elements titles = doc.select("h1");  // 查找所有<h1>元素
+        Element title = titles.first();  // 获取第一个<h1>元素
+        log.debug("Title: " + title.text());
+
+        Elements links = doc.select("a");  // 查找所有<a>元素
+        for (Element link : links) {
+            String href = link.attr("href");  // 获取<a>元素的href属性值
+            String text = link.text();  // 获取<a>元素的文本内容
+            log.debug("Link: " + text + " - " + href);
         }
     }
 }
